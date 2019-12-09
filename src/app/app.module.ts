d5de3966d663d258db8a67eaad703b4da09ea950
Nadia <mokhirevanadezda@gmail.com>
2019-12-08 23:12:09 -0500

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ItemGalleryComponent } from './components/item-gallery/item-gallery.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ItemGalleryComponent,
+    ItemDetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
