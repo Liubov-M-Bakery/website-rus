50b2822e52f444c5c06e00cfa8958a38e729f004
Nadia <liubov.m.bakery@gmail.com>
2019-12-18 22:18:14 -0500

    heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/dist'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
